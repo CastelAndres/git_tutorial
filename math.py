daa8c1f8233b709da0c413c52aa6e73c8597b0a8
Andres Castellanos <andrescastellanos@Andress-MacBook-Air.local>
2025-08-28 13:35:20 -0400

    added some comments to the add function

--- math.py
+++ math.py
@@ -1,7 +1,9 @@
 #I need to add this
 def add(x,y)
+  #if x and y are both integers, then add them
   if (type(x)== int and type(y)== int):
     return x+y
+  #otherwise return none
   return None
 #Bob needs to add this
 def mult(x,y)
